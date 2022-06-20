a4a0c5e28acbb612e0d938e488335885602f49d0
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-06-20 13:48:23 +0300

    commit on mobile-menu

---
+++ JS files/mobile.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu-3");
+
+hamburger.addEventListener('click', ()=> {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.port').forEach(n => 
+    n.addEventListener('click', () =>  {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
